b725586682a0a189bba1b3e42aeb6b03bea37fb7
Ivan Seniv <seniv97@gmail.com>
2017-12-06 12:53:14 +0200

    make 'card-front' component

---
+++ vue/src/components/CardFront.vue
@@ -0,0 +1,84 @@
+<template>
+  <li class="card" :data-card="card" :class="{red: mast == 'heart' || mast == 'diams'}" v-html="symbol"></li>
+</template>
+
+<script>
+export default {
+  name: 'card-front',
+  props: ['card', 'mast'],
+  data () {
+    return {
+    }
+  },
+  computed: {
+    symbol () {
+      switch (this.mast) {
+        case 'heart': return '&hearts;'
+        case 'diams': return '&diams;'
+        case 'spades': return '&spades;'
+        case 'clubs': return '&clubs;'
+      }
+      return ''
+    }
+  }
+}
+</script>
+
+<style>
+  li.card {
+    display: inline-block;
+    width: 100px;
+    height: 160px;
+    background-color: #eeeeee;
+    border-radius: 5px;
+    transition: .3s;
+    padding-top: 30px;
+    font-size: 80px;
+    position: relative;
+    cursor: pointer;
+    box-sizing: border-box;
+    text-align: center;
+  }
+
+  li.card.red {
+    color: #d00;
+  }
+
+  li.card:hover {
+    transform: scale(1.1);
+    box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.25);
+    z-index: 322;
+  }
+
+  li.card::before,
+  li.card::after {
+    position: absolute;
+    content: attr(data-card);
+    text-transform: uppercase;
+    font-size: 24px;
+  }
+
+  li.card::before {
+    top: 10px;
+    left: 10px;
+  }
+
+  li.card::after {
+    bottom: 10px;
+    right: 10px;
+    transform: rotate(180deg)
+  }
+  li.card:not(:first-child) {
+    margin-left: 5px;
+  }
+
+  /* @media screen and (max-width:768px) {
+    li.card:not(:first-child) {
+      margin-left: -20px;
+      box-shadow: -5px 0px 10px 0px rgba(0, 0, 0, 0.15);
+    }
+    li.card:hover {
+      box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.20);
+    }
+  } */
+</style>
---
+++ vue/src/components/Game.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+    <ul class="your-cards">
+      <card-front :mast="card.mast" :card="card.card" v-for="(card, index) in cards" :key="index"></card-front>
+    </ul>
+  </div>
+</template>
+
+<script>
+import CardFront from '@/components/CardFront.vue'
+
+export default {
+  name: 'game',
+  components: {
+    CardFront
+  },
+  data () {
+    return {
+      cards: [
+        {card: 'j', mast: 'heart'},
+        {card: 'q', mast: 'diams'},
+        {card: 'k', mast: 'spades'},
+        {card: 'a', mast: 'clubs'}
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+}
+a {
+  color: #42b983;
+}
+</style>
